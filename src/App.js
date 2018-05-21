3b483a5204c78367256e1db9779b4e38a6ee312d
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 09:41:41 -0500

    myreads

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           
           <h1 className="App-title">MyReads</h1>
         </header>
