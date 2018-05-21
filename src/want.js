8be7d597c1901e6ad4b4614696e050c19605ad12
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 12:27:42 -0500

    ant to read

--- src/want.js
+++ src/want.js
@@ -9,14 +9,14 @@ class WantToRead extends Component{
                 <hr/>
                 <div>
                     <figure>
-                        <img src="data:image/jpeg;base64," />
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/51ctyoISRHL._SY291_BO1,204,203,200_QL40_.jpg" />
                         <p>1776</p>
                         <p>David McCullough</p>
                     </figure>
                 </div>
                 <div>
                     <figure>
-                        <img src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABA" />
+                        <img src="https://i.pinimg.com/736x/5f/84/d0/5f84d040be5b825d151db173f02336cd---years-new-books.jpg" />
                         <p>Harrypotter</p>
                         <p>J.K.Rowlling</p>
                     </figure>
