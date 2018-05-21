bc0cf33394e57eb02fba5ad2663241209c22dbb3
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 10:01:06 -0500

    Currently reading

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './header.js';
+import Currently from './currently.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Currently />
       </div>
     );
   }
--- src/currently.js
+++ src/currently.js
@@ -3,7 +3,9 @@ import react,{component} from 'react'
 class Currently extends Component{
     render(){
         return(
-            
+            <h2>Currently Reading</h2>
         );
     }
 }
+
+export default Currently
