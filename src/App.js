3174d8479526444a6d14e3b8f75deef03b15e707
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 09:43:18 -0500

    header

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          
-          <h1 className="App-title">MyReads</h1>
+        <header style={{backgroundColor:"green"}}>
+            <h1 className="App-title">MyReads</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
