3f67c2fd4b140ee9632148e9b465711ccf3ff465
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 11:02:22 -0500

    Currently reading

--- src/currently.js
+++ src/currently.js
@@ -3,19 +3,21 @@ import React,{Component} from 'react'
 class Currently extends Component{
     render(){
         return(
-            <div>
+            <div >
               <h2>Currently Reading</h2>
               <hr />
-              <figure>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlSwaDrzmNCbg8-izaninusodsNJuhRrWzq4JYjx1wBgbW7CM"/>
-                <figcaption>To Kill a Mokingbird</figcaption>
-                <figcaption>Harper Lee</figcaption> 
-              </figure>
-              <figure>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROF_ntluFvB8juQD9utEJ6LG3JAXSQWeWCMc1tuJlxzC-eH0Fs"/>
-                <figcaption> Ender Games</figcaption>
-                <figcaption>Orson Scott Card</figcaption>
-              </figure>
+              <div style={{display:'inline'}}>
+                <figure>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlSwaDrzmNCbg8-izaninusodsNJuhRrWzq4JYjx1wBgbW7CM"/>
+                    <figcaption>To Kill a Mokingbird</figcaption>
+                    <figcaption>Harper Lee</figcaption> 
+                </figure>
+                <figure>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROF_ntluFvB8juQD9utEJ6LG3JAXSQWeWCMc1tuJlxzC-eH0Fs"/>
+                    <figcaption> Ender Games</figcaption>
+                    <figcaption>Orson Scott Card</figcaption>
+                </figure>
+              </div>
             </div>
         );
     }
