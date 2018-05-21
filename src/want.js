79273f574a38204512b7ed0e71a03bd4d1fc325d
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 11:32:56 -0500

    ant to read

--- src/want.js
+++ src/want.js
@@ -3,17 +3,14 @@ import React,{Component} from 'react'
 class WantToRead extends Component{
     constructor(props){
         super(props);
-        this.state=[{
-            imgurl:'"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlSwaDrzmNCbg8-izaninusodsNJuhRrWzq4JYjx1wBgbW7CM"/>',
+        this.state={
+            
+            imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlSwaDrzmNCbg8-izaninusodsNJuhRrWzq4JYjx1wBgbW7CM",
             title : '1776',
             author: 'David McCullough'
-            },
-            {
-            imgurl:'"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlSwaDrzmNCbg8-izaninusodsNJuhRrWzq4JYjx1wBgbW7CM"/>',
-            title : "Harry Potter and the Sorcerer's Stone",
-            author: 'J.K. Rowling'
-            }
-        ]
+            
+           
+        }
     }
     render(){
         return(
@@ -21,7 +18,9 @@ class WantToRead extends Component{
                 <h2>Want To Read</h2>
                 <hr/>
                 <div>
-
+                    <img src={this.state.imgurl} />
+                    <h2>{this.state.title}</h2>
+                    <p>{this.state.author}</p>
                 </div>
             </div>
         );
