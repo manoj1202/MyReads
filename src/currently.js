488f811ca1525e10160be5071993ceaec5454deb
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-21 10:15:49 -0500

    Currently reading

--- src/currently.js
+++ src/currently.js
@@ -6,9 +6,15 @@ class Currently extends Component{
             <div>
               <h2>Currently Reading</h2>
               <hr />
+              <figure>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStlSwaDrzmNCbg8-izaninusodsNJuhRrWzq4JYjx1wBgbW7CM"/>
+                <figcaption>To Kill a Mokingbird</figcaption> 
+                </figure>
             </div>
         );
     }
 }
 
 export default Currently
+
+//  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROF_ntluFvB8juQD9utEJ6LG3JAXSQWeWCMc1tuJlxzC-eH0Fs"/>
